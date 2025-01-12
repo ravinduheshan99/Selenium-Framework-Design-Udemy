f85c7dc343216de5226c2bbd3f412c0552aa3fdf
ravinduheshan99 <ravinduheshan99@gmail.com>
2025-01-12 19:11:19 +0530

    Implementing parameterization into tests with testng data provider

--- src/test/java/ravinduheshan99/tests/StandAloneTestModified.java
+++ src/test/java/ravinduheshan99/tests/StandAloneTestModified.java
@@ -2,6 +2,7 @@ package ravinduheshan99.tests;
 
 import java.io.IOException;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ravinduheshan99.TestComponents.BaseTest;
 import ravinduheshan99.pageobjects.CartPage;
@@ -13,9 +14,9 @@ public class StandAloneTestModified extends BaseTest{
 	
 	String productName = "ADIDAS ORIGINAL";
 
-	@Test
-	public void placeOrder() throws InterruptedException, IOException{
-		ProductCatalogue productCatalogue = landingPage.loginApplication("test@gmail.com", "test@123");
+	@Test(dataProvider="getData", groups= {"Purchase"})
+	public void placeOrder(String email, String password, String productName) throws InterruptedException, IOException{
+		ProductCatalogue productCatalogue = landingPage.loginApplication(email, password);
 		productCatalogue.getProductList();
 		productCatalogue.addPoductToCart(productName);
 		CartPage cartPage = productCatalogue.goToCartPage();
@@ -31,5 +32,10 @@ public class StandAloneTestModified extends BaseTest{
 		ProductCatalogue productCatalogue = landingPage.loginApplication("test@gmail.com", "test@123");
 		OrderPage orderPage = productCatalogue.goToOrdersPage();
 		Assert.assertTrue(orderPage.VerifyOrderDisplay(productName));
+	}
+	
+	@DataProvider
+	public Object[][] getData() {
+		return new Object[][] {{"test@gmail.com","test@123","ADIDAS ORIGINAL"},{" raviya@gmail.com","Raviya@123","IPHONE 13 PRO"}};
 	}
 }
