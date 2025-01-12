9ce21bd4651cae442738a70947838ba696661fd6
ravinduheshan99 <ravinduheshan99@gmail.com>
2025-01-12 17:05:39 +0530

    Implement test stratergy for framework on how tests are divided based on modules

--- src/test/java/ravinduheshan99/tests/ErrorValidations.java
+++ src/test/java/ravinduheshan99/tests/ErrorValidations.java
@@ -4,11 +4,23 @@ import java.io.IOException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ravinduheshan99.TestComponents.BaseTest;
+import ravinduheshan99.pageobjects.CartPage;
+import ravinduheshan99.pageobjects.ProductCatalogue;
 public class ErrorValidations extends BaseTest{
 
 	@Test
-	public void placeOrder() throws InterruptedException, IOException{
+	public void LoginErrorValidation() throws InterruptedException, IOException{
 		landingPage.loginApplication("test@gmail.com", "Invalid");
 		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+	}
+	
+	@Test
+	public void ProductErrorValidation() throws InterruptedException, IOException{
+		String productName = "ADIDAS ORIGINAL";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("test@gmail.com", "test@123");
+		productCatalogue.getProductList();
+		productCatalogue.addPoductToCart(productName);
+		CartPage cartPage = productCatalogue.goToCartPage();
+		Assert.assertFalse(cartPage.VerifyProductDisplay("Invalid Product Name"));
 	}
 }
