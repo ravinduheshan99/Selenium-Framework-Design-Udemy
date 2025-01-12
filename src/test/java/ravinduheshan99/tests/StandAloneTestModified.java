5784bae6092acd26c2e27fbff41b500ecec09af8
ravinduheshan99 <ravinduheshan99@gmail.com>
2025-01-12 22:23:08 +0530

    Integration of hashmap to data provider to send the data as one hash object

--- src/test/java/ravinduheshan99/tests/StandAloneTestModified.java
+++ src/test/java/ravinduheshan99/tests/StandAloneTestModified.java
@@ -1,6 +1,8 @@
 package ravinduheshan99.tests;
 
 import java.io.IOException;
+import java.util.HashMap;
+
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -15,12 +17,12 @@ public class StandAloneTestModified extends BaseTest{
 	String productName = "ADIDAS ORIGINAL";
 
 	@Test(dataProvider="getData", groups= {"Purchase"})
-	public void placeOrder(String email, String password, String productName) throws InterruptedException, IOException{
-		ProductCatalogue productCatalogue = landingPage.loginApplication(email, password);
+	public void placeOrder(HashMap<String,String> map) throws InterruptedException, IOException{
+		ProductCatalogue productCatalogue = landingPage.loginApplication(map.get("email"), map.get("password"));
 		productCatalogue.getProductList();
-		productCatalogue.addPoductToCart(productName);
+		productCatalogue.addPoductToCart(map.get("productName"));
 		CartPage cartPage = productCatalogue.goToCartPage();
-		Assert.assertTrue(cartPage.VerifyProductDisplay(productName));
+		Assert.assertTrue(cartPage.VerifyProductDisplay(map.get("productName")));
 		CheckoutPage checkoutPage = cartPage.goToCheckoutPage();
 		checkoutPage.selectCountry("Sri");
 		ConfirmationPage confirmationPage = checkoutPage.submitOrder();
@@ -36,6 +38,16 @@ public class StandAloneTestModified extends BaseTest{
 	
 	@DataProvider
 	public Object[][] getData() {
-		return new Object[][] {{"test@gmail.com","test@123","ADIDAS ORIGINAL"},{" raviya@gmail.com","Raviya@123","IPHONE 13 PRO"}};
+		HashMap<String,String> map1 = new HashMap<String,String>();
+		map1.put("email", "test@gmail.com");
+		map1.put("password", "test@123");
+		map1.put("productName", "ADIDAS ORIGINAL");
+		
+		HashMap<String,String> map2 = new HashMap<String,String>();
+		map2.put("email", "raviya@gmail.com");
+		map2.put("password", "Raviya@123");
+		map2.put("productName", "IPHONE 13 PRO");
+		
+		return new Object[][] {{map1},{map2}};
 	}
 }
