349fdc55a0880d22bf840c40dae24666147f8a50
ravinduheshan99 <ravinduheshan99@gmail.com>
2025-03-23 02:08:00 +0530

    Integrating cucumber wrapper into selenium framework

---
+++ src/test/java/cucumber/StandAloneTestModified.feature
@@ -0,0 +1,17 @@
+@tag
+Feature: Purchase the Order from Ecommerce Website
+  I want to use this template for my feature file
+
+  Background:
+  Given I Landed on Ecommerce Page
+
+  @tag2
+  Scenario Outline: Positive Test of Submitting the order
+    Given Logged in with username <name> and password <password>
+    When I add product <productName> from Cart
+    And Checkout <productName> and submit the order
+    Then "THANKYOU FOR THE ORDER." message is displayed on ConfirmationPage
+
+    Examples: 
+      | name           | password | productName |
+      | test@gmail.com | test123 | ZARA COAT 3 |
---
+++ src/test/java/ravinduheshan99/stepDefinitions/StepDefinitionImpl.java
@@ -0,0 +1,55 @@
+package ravinduheshan99.stepDefinitions;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import ravinduheshan99.TestComponents.BaseTest;
+import ravinduheshan99.pageobjects.CartPage;
+import ravinduheshan99.pageobjects.CheckoutPage;
+import ravinduheshan99.pageobjects.ConfirmationPage;
+import ravinduheshan99.pageobjects.LandingPage;
+import ravinduheshan99.pageobjects.ProductCatalogue;
+
+public class StepDefinitionImpl extends BaseTest {
+
+	public LandingPage landingPage;
+	public ProductCatalogue productCatalogue;
+	public CartPage cartPage;
+	public CheckoutPage checkoutPage;
+	public ConfirmationPage confirmationPage;
+
+	@Given("I Landed on Ecommerce Page")
+	public void I_Landed_on_Ecommerce_Page() throws IOException {
+		landingPage = launchApplication();
+	}
+
+	@Given("^Logged in with username (.+) and password (.+)$")
+	public void Logged_in_with_username_and_password(String username, String password) {
+		productCatalogue = landingPage.loginApplication(username, password);
+	}
+
+	@When("^I add product (.+) from Cart$")
+	public void I_add_product_from_Cart(String productName) throws InterruptedException {
+		productCatalogue.getProductList();
+		productCatalogue.addPoductToCart(productName);
+	}
+
+	@And("^Checkout (.+) and submit the order$")
+	public void Checkout_and_submit_the_order(String productName) {
+		cartPage = productCatalogue.goToCartPage();
+		Assert.assertTrue(cartPage.VerifyProductDisplay(productName));
+		checkoutPage = cartPage.goToCheckoutPage();
+		checkoutPage.selectCountry("Sri");
+	}
+
+	@Then("{string} message is displayed on ConfirmationPage")
+	public void message_displayed_confirmationPage(String string) {
+		confirmationPage = checkoutPage.submitOrder();
+		Assert.assertTrue(confirmationPage.getConfirmationMessage().equalsIgnoreCase(string));
+		driver.close();
+	}
+}
